8c751048e47d5cf49998eba6ced941479f1c180b
Jashwanth R <74175294+Noir666@users.noreply.github.com>
2022-09-29 08:48:17 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Good
 {
 static void swap(int m,  int n)
@@ -9,7 +10,12 @@ System.out.println("the value of m"+ m +" and the value of n is "+ n);
 }
 public static void main(String[] args)
 {
-int m=10, n=29;
+System.out.print("Enter the first number :");
+Scanner sd = new Scanner(System.in);
+int m=sd.nextInt();
+System.out.print("Enter the secoond number :");
+Scanner sd = new Scanner(System.in);
+int n=sd.nextInt();
 swap(m, n);
 }
 }
