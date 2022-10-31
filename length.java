6153e16455e92587dd7e7d3d58a8e7f6a5666b18
Jashwanth R <74175294+Noir666@users.noreply.github.com>
2022-10-31 09:21:43 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class length
+{
+public static void main(String[] args)
+{
+System.out.print("enter the string :");
+Scanner a=new Scanner(System.in);
+String b=a.nextLine();
+System.out.print("the length of the "+b+"is "+b.length());
+}
+}
